35c1220cc7dba142b924ea143baffd20232ae50f
Sokil Natalia <sokilnatalka@gmail.com>
2022-12-28 16:31:18 +0200

    changed Exception and command name

--- bot_helper.py
+++ bot_helper.py
@@ -3,10 +3,7 @@ def input_error(func):
         try:
             return func(*args)
         except IndexError:
-            return "Sorry, reading from invalid index"
-        except Exception as e:
-            return str(e)
-
+            return "Sorry try again"
 
     return wrapper
 
@@ -16,11 +13,8 @@ contacts = {}
 
 @input_error
 def add(*args):
-    try:
-        name = args[0]
-        phone_number = args[1]
-    except IndexError:
-        raise Exception("Please, input name and phone")
+    name = args[0]
+    phone_number = args[1]
     contacts[name] = phone_number
     return f"This is ADD, name {name}, phone {phone_number}"
 
@@ -31,28 +25,17 @@ def hello(*args):
 
 @input_error
 def change(*args):
-    try:
-        name = args[0]
-        phone_number = args[1]
-    except IndexError:
-        raise Exception("Please, input name and phone")
-
+    name = args[0]
+    phone_number = args[1]
     if name in contacts.keys():
         contacts[name] = phone_number
-        return f"This is CHANGE, phone {phone_number} for name {name}"
-    else:
-        raise Exception(f"This name {name} is not found. Please input correct name")
+    return f"This is CHANGE, phone {phone_number} for name {name}"
 
 @input_error
 def phone(*args):
-    try:
-        name = args[0]
-    except IndexError:
-        raise Exception("Please specify namebot_helper.py")
+    name = args[0]
     if name in contacts.keys():
         return f"This is phone {contacts[name]} for name {name}"
-    else:
-        raise Exception(f"This name {name} is not found. Please input correct name")
 
 
 def show_all(*args):
@@ -71,22 +54,18 @@ COMMANDS = {
     "hello": hello,
     "change": change,
     "phone": phone,
-    "show_all": show_all,
+    "show all": show_all,
     "close": close,
-    "good_bye": close,
+    "good bye": close,
     "exit": close,
 }
 
-
 def command_parser(user_input: str):
-    chunks = user_input.strip().split(" ")
-    command_name = chunks[0].lower()
-    args = chunks[1:]
+    for  key_word, command in COMMANDS.items():
+        if user_input.startswith(key_word):
+            return command, user_input.replace(key_word, "").strip().split(" ")
+    return None, None
 
-    if command_name in COMMANDS.keys():
-        return COMMANDS[command_name], args
-    else:
-        return None, None
 
 
 def main():
